a1c62747d57bc3957e4f2387a1ca468c95bb6de1
Jordan Beattie <jordan@mission10.co.uk>
2022-03-17 16:09:49 +0000

    Added standard properties by default on entry models

--- src/models/Entry.php
+++ src/models/Entry.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Jordanbeattie\CraftcmsFaker\models;
+use DateTime;
 use Jordanbeattie\CraftcmsFaker\models\Asset;
 use Jordanbeattie\CraftcmsFaker\models\Collection;
 
@@ -26,7 +27,28 @@ class Entry extends Collection
         {
             $class->$key = $value;
         }
+        $this->addStandardProperties($class);
         return $class;
+    }
+
+    private function addStandardProperties($class)
+    {
+
+        $defaults = [
+            'title' => 'Fake entry', 
+            'postDate' => date('now'), 
+            'slug' => 'fake-entry', 
+            'url' => '#'
+        ];
+
+        foreach ($defaults as $property => $value) {
+            if(!property_exists($class, $property))
+            {
+                $class->$property = $value;
+            }
+        }
+
+
     }
 
 }
