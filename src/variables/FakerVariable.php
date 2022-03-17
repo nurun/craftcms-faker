ed618a8406971738509c61f8a4812408537c851c
Jordan Beattie <jordan@mission10.co.uk>
2022-03-17 16:10:15 +0000

    Added width and height options to asset model

--- src/variables/FakerVariable.php
+++ src/variables/FakerVariable.php
@@ -14,10 +14,16 @@ Class FakerVariable
     
     public function asset( $attributes = null )
     {
-        $url = $attributes['url'] ?? 'https://picsum.photos/200';
+        $fakeUrl = 'https://picsum.photos/200/200';
+        if( !is_null($attributes) && array_key_exists('width', $attributes) && array_key_exists('height', $attributes) )
+        {
+            $fakeUrl = "https://picsum.photos/" . $attributes['width'] . "/" . $attributes['height'];
+        }
+        $url = $attributes['url'] ?? $fakeUrl;
         $title = $attributes['title'] ?? 'Default image title';
         $alt = $attributes['alt'] ?? $title;
         $kind = $attributes['kind'] ?? null;
+
         return new Asset($url, $title, $alt, $kind);
     }
 
